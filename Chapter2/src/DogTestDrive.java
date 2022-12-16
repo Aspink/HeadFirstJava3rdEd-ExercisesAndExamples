7cf26b19b06bd84859e1d3634e5c0a08916031c5
Aspink <aspink1@wp.pl>
2022-12-16 21:23:06 +0100

    Chapter2 added with Dog and DogTestDrive classes

---
+++ Chapter2/src/DogTestDrive.java
@@ -0,0 +1,9 @@
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog velvet = new Dog();
+        velvet.size = 40;
+        velvet.name = "Velvet";
+        velvet.breed = "Mix";
+        velvet.bark();
+    }
+}
