9d4329011de9a45713beae82b408cc553709caac
Aspink <aspink1@wp.pl>
2022-12-17 21:45:02 +0100

    Episode + test clases added

---
+++ Chapter2/src/Episode.java
@@ -0,0 +1,15 @@
+public class Episode {
+
+    int seriesNumber;
+    int episodeNumber;
+
+    void play() {
+        System.out.println("Playing serie " + seriesNumber);
+    }
+    void skipIntro() {
+        System.out.println("Skipping intro...");
+    }
+    void skipToNext() {
+        System.out.println("Loading next episode...");
+    }
+}
---
+++ Chapter2/src/EpisodeTestDrive.java
@@ -0,0 +1,8 @@
+public class EpisodeTestDrive {
+    public static void main(String[] args) {
+        Episode episode = new Episode();
+        episode.seriesNumber = 4;
+        episode.play();
+        episode.skipIntro();
+    }
+}
