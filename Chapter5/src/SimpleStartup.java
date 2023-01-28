077ad5ff6a6ac26357d38fa4cf9b56f05050f301
Aspink <aspink1@wp.pl>
2023-01-28 21:51:34 +0100

    methode checkYourself in SimpleStartup class finished.

--- Chapter5/src/SimpleStartup.java
+++ Chapter5/src/SimpleStartup.java
@@ -6,5 +6,19 @@ public class SimpleStartup {
     }
 
     public String checkYourself(int guess) {
+        String result = "miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits ==
+                locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result;
     }
 }
