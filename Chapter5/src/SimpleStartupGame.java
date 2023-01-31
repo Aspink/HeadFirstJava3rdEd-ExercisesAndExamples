7c0dca877417d2a2a242a35e629616d24c828dad
Aspink <aspink1@wp.pl>
2023-01-31 21:27:41 +0100

    SimpleStartupGame in construction.

--- Chapter5/src/SimpleStartupGame.java
+++ Chapter5/src/SimpleStartupGame.java
@@ -1,3 +1,15 @@
 public class SimpleStartupGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
 
+        SimpleStartup theStartup = new SimpleStartup();
+        int randomNum = (int) (Math.random() * 5);
+
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theStartup.setLocationCells(locations);
+        boolean isAlive = true;
+
+
+    }
 }
