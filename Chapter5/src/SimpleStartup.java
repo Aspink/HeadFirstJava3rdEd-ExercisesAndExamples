7aed4e86847ae565cc94c1674c1a746ec26424aa
Aspink <aspink1@wp.pl>
2023-03-12 20:42:42 +0100

    SimpleStartup modification

--- Chapter5/src/SimpleStartup.java
+++ Chapter5/src/SimpleStartup.java
@@ -2,6 +2,7 @@ public class SimpleStartup {
     private int[] locationCells;
     private int numOfHits = 0;
     public void setLocationCells(int[] locs) {
+
         locationCells = locs;
     }
 
