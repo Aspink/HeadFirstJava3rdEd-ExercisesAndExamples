16e8249453ea8a19a2f3d159744257922d0691e6
Aspink <aspink1@wp.pl>
2023-03-14 21:37:05 +0100

    instance of SimpleStratup created and some other newbie in SimpleStartupOwnGame class

--- Chapter5/src/SimpleStartupOwnGame.java
+++ Chapter5/src/SimpleStartupOwnGame.java
@@ -1,6 +1,12 @@
+import java.util.Random;
 public class SimpleStartupOwnGame {
     public static void main(String[] args) {
         int numOfGuesses = 0;
+        Random random = new Random();
+        int startCell = random.nextInt(4);
+        int[] cellSet = new int[]{startCell, startCell + 1, startCell + 2};
+        SimpleStartup startup = new SimpleStartup();
+        startup.setLocationCells(cellSet);
 
     }
 
