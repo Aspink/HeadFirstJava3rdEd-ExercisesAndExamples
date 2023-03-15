440866e017125de2b1f423f4d6d208d9b54aa447
Aspink <aspink1@wp.pl>
2023-03-15 18:43:18 +0100

    GameHelper class finished and tested

--- Chapter5/src/GameHelper.java
+++ Chapter5/src/GameHelper.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class GameHelper {
-    public int getUserInput(String enterANumber) {
-        return 1;
+    public int getUserInput(String prompt) {
+        System.out.print(prompt + ": ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
