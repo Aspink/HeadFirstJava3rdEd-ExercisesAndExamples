00b11231e5ba3826e983931be61e0206e69e94cf
Aspink <aspink1@wp.pl>
2023-03-15 16:04:55 +0100

    SimpleStartupOwnGame class finished and tested

--- Chapter5/src/SimpleStartupOwnGame.java
+++ Chapter5/src/SimpleStartupOwnGame.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 public class SimpleStartupOwnGame {
     public static void main(String[] args) {
         int numOfGuesses = 0;
@@ -7,10 +8,15 @@ public class SimpleStartupOwnGame {
         int[] cellSet = new int[]{startCell, startCell + 1, startCell + 2};
         SimpleStartup startup = new SimpleStartup();
         startup.setLocationCells(cellSet);
-        String status
+        String status = "";
         while(!status.equals("kill")) {
-
+            Scanner input = new Scanner(System.in);
+            System.out.print("What is your shot (0-6)?");
+            int shot = input.nextInt();
+            numOfGuesses++;
+            status = startup.checkYourself(shot);
         }
+        System.out.println("You used " + numOfGuesses + " shots to sink the startup.");
 
     }
 
