e4bfda99602fd54f807890711d5bffa912cccdff
Aspink <aspink1@wp.pl>
2023-03-17 19:57:44 +0100

    SimpleStartup class finished

--- Chapter6/src/SimpleStartup.java
+++ Chapter6/src/SimpleStartup.java
@@ -4,23 +4,20 @@ public class SimpleStartup {
     private ArrayList<String> locationCells;
 //    private int numOfHits = 0;
     public void setLocationCells(ArrayList<String> locs) {
-
         locationCells = locs;
     }
 
     public String checkYourself(String userInput) {
         String result = "miss";
-        for (int cell : locationCells) {
-            if (guess == cell) {
+        int index = locationCells.indexOf(userInput);
+        if(index >= 0) {
+            locationCells.remove(index);
+            if(locationCells.isEmpty()) {
+                result = "kill";
+            } else {
                 result = "hit";
-                numOfHits++;
-                break;
             }
         }
-        if (numOfHits == locationCells.length) {
-            result = "kill";
-        }
-        System.out.println(result);
         return result;
     }
 }
