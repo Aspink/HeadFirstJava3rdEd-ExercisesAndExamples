623e0f9719c1883de43f9579ee2c153a98df0164
Aspink <aspink1@wp.pl>
2023-03-18 18:35:40 +0100

    Variables declared and initialized in StartupBust

--- Chapter6/src/StartupBust.java
+++ Chapter6/src/StartupBust.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+
 public class StartupBust {
+    private GameHelper helper = new GameHelper();
+
+    private ArrayList<Startup> startups = new ArrayList<Startup>();
+    private int numOfGuesses = 0;
     public static void main(String[] args) {
 
     }
