7afaa935163f3c85ebc6033d7e98aa482dc3768e
Aspink <aspink1@wp.pl>
2023-03-19 21:07:43 +0100

    Method placeStartup created in GameHelper class

--- Chapter6/src/GameHelper.java
+++ Chapter6/src/GameHelper.java
@@ -16,4 +16,28 @@ public class GameHelper {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine().toLowerCase();
     }
+
+    public ArrayList<String> placeStartup(int startupSize) {
+        int[] startupCoords = new int[startupSize]; // current candidate co-ordinates
+        int attempts = 0; // current attempts counter
+        boolean success = false; // flag = found a good location?
+
+        startupCount++; // nth Startup to place
+        int increment = getIncrement (); // alternate vert & horiz alignment
+        while (!success & attempts++ < MAX_ATTEMPTS) {
+            int location = random.nextInt (GRID_SIZE); // get random starting point
+
+            for (int i = 0; i < startupCoords.length; i++) { // create array of proposed coords
+                startupCoords[i] = location; // put current location in array
+                location += increment; // calculate the next location
+            }
+            // system.out.println("Trying: " + Arrays.toString(startupCoords) );
+            if (startupFits(startupCoords, increment)) { // startup fits on the grid?
+                success = coordsAvailable(startupCoords) ; // ...and locations aren't taken?
+            }
+        }
+        savePositionToGrid(startupCoords);
+        ArrayList<String> alphaCells = convertCoordsToAlphaFormat(startupCoords);
+        return alphaCells;
+    }
 }
