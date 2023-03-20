0a8080d8cdb7fed5622d00e38e5e8bb5329e69cb
Aspink <aspink1@wp.pl>
2023-03-20 21:45:12 +0100

    Method savePositionToGrid created in GameHelper class

--- Chapter6/src/GameHelper.java
+++ Chapter6/src/GameHelper.java
@@ -18,22 +18,21 @@ public class GameHelper {
     }
 
     public ArrayList<String> placeStartup(int startupSize) {
-        int[] startupCoords = new int[startupSize]; // current candidate co-ordinates
-        int attempts = 0; // current attempts counter
-        boolean success = false; // flag = found a good location?
-
-        startupCount++; // nth Startup to place
-        int increment = getIncrement (); // alternate vert & horiz alignment
+        int[] startupCoords = new int[startupSize];         // current candidate co-ordinates
+        int attempts = 0;                                   // current attempts counter
+        boolean success = false;                            // flag = found a good location?
+        startupCount++;                                     // nth Startup to place
+        int increment = getIncrement ();                    // alternate vert & horiz alignment
         while (!success & attempts++ < MAX_ATTEMPTS) {
-            int location = random.nextInt (GRID_SIZE); // get random starting point
+            int location = random.nextInt (GRID_SIZE);      // get random starting point
 
             for (int i = 0; i < startupCoords.length; i++) { // create array of proposed coords
-                startupCoords[i] = location; // put current location in array
-                location += increment; // calculate the next location
+                startupCoords[i] = location;                 // put current location in array
+                location += increment;                       // calculate the next location
             }
             // system.out.println("Trying: " + Arrays.toString(startupCoords) );
-            if (startupFits(startupCoords, increment)) { // startup fits on the grid?
-                success = coordsAvailable(startupCoords) ; // ...and locations aren't taken?
+            if (startupFits(startupCoords, increment)) {    // startup fits on the grid?
+                success = coordsAvailable(startupCoords) ;  // ...and locations aren't taken?
             }
         }
         savePositionToGrid(startupCoords);
@@ -43,13 +42,12 @@ public class GameHelper {
 
     private boolean startupFits(int[] startupCoords, int increment) {
         int finalLocation = startupCoords[startupCoords.length - 1];
-        if (increment == HORIZONTAL_INCREMENT) {
-            // check end is on same row as start
+        if (increment == HORIZONTAL_INCREMENT) {            // check end is on same row as start
             return calcRowFromIndex(startupCoords[0]) == calcRowFromIndex (finalLocation);
         } else {
-            return finalLocation < GRID_SIZE; // check end isn't off the bottom
+            return finalLocation < GRID_SIZE;               // check end isn't off the bottom
         }
-    } //end startupFits
+    }          //end startupFits
 
     private boolean coordsAvailable(int[] startupCoords) {
         for (int coord : startupCoords) {                   // check all potential positions
@@ -60,4 +58,10 @@ public class GameHelper {
         }
         return true;                                        // there were no clashes, yay
     }           //end coordsAvailable
+
+    private void savePositionToGrid(int[] startupCoords) {
+        for (int index : startupCoords) {
+            grid[index] = 1;                                // mark grid position as ‘used'
+        }
+    }      //end savePositionToGrid
 }
