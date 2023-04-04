3d7a956cb80cfa04b2bd871ba26e599e011b10ef
Aspink <aspink1@wp.pl>
2023-04-04 19:12:46 +0200

    HFJch6#06 2 removed, "four" added.

--- Chapter6/src/ArrayListMagnet.java
+++ Chapter6/src/ArrayListMagnet.java
@@ -7,6 +7,11 @@ public class ArrayListMagnet {
         a.add(2, "two");
         a.add(3, "three");
         printList(a);
+        a.remove(2);
+        if(a.contains("three")) {
+            a.add("four");
+        }
+        printList(a);
     }
 
     public static void printList(ArrayList<String> list) {
