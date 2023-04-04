d1eede7fc5fd458c7c9d63795a9fc1b1d83105ba
Aspink <aspink1@wp.pl>
2023-04-04 18:21:05 +0200

    HFJch6#03 ArrayList a created and filled from 0 to 3.

--- Chapter6/src/ArrayListMagnet.java
+++ Chapter6/src/ArrayListMagnet.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
 public class ArrayListMagnet {
     public static void main(String[] args) {
-
+        ArrayList<String> a = new ArrayList<String>();
+        a.add(0, "zero");
+        a.add(1, "one");
+        a.add(2, "two");
+        a.add(3, "three");
+        printList(a);
     }
 }
