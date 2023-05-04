8aab918ecb0cf433b2c233a467f23a54a5358ff8
Aspink <aspink1@wp.pl>
2023-05-04 20:46:54 +0200

    HFJch7#34 Mixed2 class in version 4.

--- Chapter7/src/Mixed2.java
+++ Chapter7/src/Mixed2.java
@@ -4,8 +4,8 @@ public class Mixed2 {
         B b = new B();
         C c = new C();
         A a2 = new C();
-        c.m1();
-        c.m2();
-        c.m3();
+        a2.m1();
+        a2.m2();
+        a2.m3();
     }
 }
