37d80f7d1e6befdbe40fbea08b3f30d33a6e0771
Aspink <aspink1@wp.pl>
2023-05-07 19:10:06 +0200

    HFJch8#04 AnimalTestDrive class fixed.

--- Chapter8/src/AnimalTestDrive.java
+++ Chapter8/src/AnimalTestDrive.java
@@ -1,8 +1,8 @@
 public class AnimalTestDrive {
     public static void main(String[] args) {
         MyAnimalList list = new MyAnimalList();
-        Dog dog = new Dog;
-        Cat cat = new Cat;
+        Dog dog = new Dog();
+        Cat cat = new Cat();
         list.add(dog);
         list.add(cat);
     }
